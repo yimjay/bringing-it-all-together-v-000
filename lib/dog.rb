95de3fd13f987b8f7504dcc21d381c88018fcab7
Ji Yim <gsooyim@gmail.com>
2018-05-29 16:08:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,7 @@ class Dog
       new_dog = self.create(name: name, breed: breed)
     else
       dog_info = new_dog[0]
-      new_dog = Dog.new(id: new_dog[0], name: new_dog[1], breed: new_dog[2])
+      new_dog = Dog.new_from_db(dog_info)
     end
   end
   
